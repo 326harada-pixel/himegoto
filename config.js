f35c3751f4414ca2d5abf42ac3d32433a947332a
326harada-pixel <326harada@gmail.com>
2025-10-16 23:57:07 +0900

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,18 @@
-
-/* himegoto - Firebase config (build010) */
+// himegoto - Firebase config (build012 full)
 window.FIREBASE_CONFIG = {
   apiKey: "AIzaSyD-gLP5rIErS678UewraA0vt59JbLZpzhU",
   authDomain: "himegoto-web.firebaseapp.com",
   projectId: "himegoto-web",
   storageBucket: "himegoto-web.firebasestorage.app",
-  messagingSenderId: "368382081243",
-  appId: "1:368382081243:web:09e3827701cbc6d1d2f4fe"
+  messagingSenderId: "363882081243",
+  appId: "1:363882081243:web:09e3827701cbc6d1d2f4fe"
 };
+
+// Auto init hook for non-module usage (safely no-op if not present)
+try {
+  if (typeof initializeApp === 'function') {
+    initializeApp(window.FIREBASE_CONFIG);
+  }
+} catch (e) {
+  /* ignore - module init will happen in firebase-auth-modal.js */
+}
