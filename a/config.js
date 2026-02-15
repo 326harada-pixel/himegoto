b7e630ad4a52d0707116ef9cbd393f63faf37964
326harada-pixel <326harada@gmail.com>
2026-02-16 02:52:21 +0900

    Delete a directory

--- a/config.js
+++
@@ -1,9 +0,0 @@
-// himegoto - Firebase config (build013 full)
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD-gLP5rIErS678UewraA0vt59JbLZpzhU",
-  authDomain: "himegoto-web.firebaseapp.com",
-  projectId: "himegoto-web",
-  storageBucket: "himegoto-web.firebasestorage.app",
-  messagingSenderId: "363882081243",
-  appId: "1:363882081243:web:09e3827701cbc6d1d2f4fe"
-};
